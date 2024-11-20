0858718105c352216dd5c101125f0065c48ddc77
Tomer <github@tomer.nahum.org>
2024-11-20 18:43:48 -0500

    more work

--- stopwatch-tracker-react/src/lib/Stopwatch.tsx
+++ stopwatch-tracker-react/src/lib/Stopwatch.tsx
@@ -1,26 +1,44 @@
+import { useEffect, useState } from "react";
+import { store, UiReactWithSchemas } from "./tinybase-store";
+const { useCell } = UiReactWithSchemas;
 
-import {store, UiReactWithSchemas} from "./tinybase-store"
-const {useCell} = UiReactWithSchemas
+const REFRESH_RATE = 16; // milliseconds. //can make this into a editable setting
 
 export function Stopwatch(params: { stopwatchId: string }) {
-    let title = useCell("stopwatches", params.stopwatchId, "title")!
-    let startTime = useCell("stopwatches", params.stopwatchId, "startTime")!
-    let paused = useCell("stopwatches", params.stopwatchId, "paused")!
-    let pausedTimeCount = useCell("stopwatches", params.stopwatchId, "pausedTimeCount")!
-    let lastPausedTime = useCell("stopwatches", params.stopwatchId, "lastPausedTime")!
+    let title = useCell("stopwatches", params.stopwatchId, "title")!;
+    let startTime = useCell("stopwatches", params.stopwatchId, "startTime")!;
+    let paused = useCell("stopwatches", params.stopwatchId, "paused")!;
+    let pausedTimeCount = useCell(
+        "stopwatches",
+        params.stopwatchId,
+        "pausedTimeCount"
+    )!;
+    let lastPausedTime = useCell(
+        "stopwatches",
+        params.stopwatchId,
+        "lastPausedTime"
+    )!;
 
-    let currentTime = Date.now();
-    let elapsedTimeMillis = paused ? lastPausedTime! - startTime - pausedTimeCount
-        : currentTime - startTime - pausedTimeCount
+    let [currentTime, setCurrentTime] = useState(Date.now());
+    let elapsedTimeMillis = paused
+        ? lastPausedTime! - startTime - pausedTimeCount
+        : currentTime - startTime - pausedTimeCount;
 
-
+    useEffect(() => {
+        const interval = setInterval(
+            () => setCurrentTime(Date.now()),
+            REFRESH_RATE
+        );
+        return () => {
+            clearInterval(interval);
+        };
+    }, []);
 
     function onDeleteButtonPressed() {
-        const confirmDelete = confirm(`Delete "${title}?"`)
-        if (!confirmDelete)
-            return
+        const confirmDelete = confirm(`Delete "${title}?"`);
+        if (!confirmDelete) return;
 
-        store.delRow("stopwatches", params.stopwatchId)
+        store.delRow("stopwatches", params.stopwatchId);
     }
 
     return (
@@ -37,41 +55,116 @@ export function Stopwatch(params: { stopwatchId: string }) {
                 X
             </button>
 
-
             <div className="pt-3"></div>
 
-            <p className="time">
-                <TimeDisplay elapsedTimeMillis={elapsedTimeMillis} />
-            </p>
+            <TimeDisplay elapsedTimeMillis={elapsedTimeMillis} />
 
-            <button onClick={()=> store.setCell("stopwatches", params.stopwatchId, "paused", false)}>
+            <div class="pt-3"></div>
+
+            <button
+                onClick={() =>
+                    store.setCell("stopwatches", params.stopwatchId, "paused", false)
+                }
+            >
                 start
             </button>
         </div>
-    )
+    );
 }
 
 function TimeDisplay(params: { elapsedTimeMillis: number }) {
+    let elapsedTimeMillis = params.elapsedTimeMillis;
+    let elapsedTimeUnits = msToUnits(elapsedTimeMillis);
+    let timeDisplay = unitsToDisplayStrings(elapsedTimeUnits);
 
-    let elapsedTimeMillis = params.elapsedTimeMillis
-    let elapsedTimeUnits = msToUnits(elapsedTimeMillis)
-    let timeDisplay = unitsToDisplayStrings(elapsedTimeUnits)
+    function Colon(params: { className?: string } = { className: "" }) {
+        return (
+            <span className={`${params.className} font-['Inter_Variable']`}>:</span>
+        );
+    }
 
     return (
         // todo tidy/change classNames
-        <p className="border-2 border-black dark:border-neutral-300 rounded-md text-5xl px-4 py-1 bg-stone-300 dark:bg-stone-600 w-64 text-center font-sans m-auto w-fit">
-            {(elapsedTimeUnits.days > 0) &&
-                <span>{timeDisplay.days}:</span>
-            }
-            {timeDisplay.hours}:{timeDisplay.minutes}:{timeDisplay.seconds}
+        <p className="border-2 border-black dark:border-neutral-300 rounded-md text-5xl px-4 py-1 bg-stone-300 dark:bg-stone-600 w-fit  text-center m-auto font-['Noto_Mono']">
+            {elapsedTimeUnits.days > 0 && (
+                <span>
+                    {timeDisplay.days}
+                    <Colon />
+                </span>
+            )}
+            {timeDisplay.hours}
+            <Colon className={""} />
+            {timeDisplay.minutes}
+            <Colon />
+            {timeDisplay.seconds}
             <span className="text-base">.</span>
             <span className="text-xs">{timeDisplay.milliseconds}</span>
-
-
         </p>
-    )
+    );
 }
 
+function StopwatchButtons(params: {
+    stopwatchId: string;
+    elapsedTimeMillis: number;
+}) {
+    const paused = useCell("stopwatches", params.stopwatchId, "paused")!;
+    const lastPausedTime = useCell(
+        "stopwatches",
+        params.stopwatchId,
+        "lastPausedTime"
+    )!;
+    const pausedTimeCount = useCell(
+        "stopwatches",
+        params.stopwatchId,
+        "pausedTimeCount"
+    )!;
+
+    function unpause() {
+        const now = Date.now();
+        if (lastPausedTime === -1) throw new Error("Timer is not paused");
+
+        const newTime = pausedTimeCount + (now - lastPausedTime);
+        store.transaction(() => {
+            store.setCell(
+                "stopwatches",
+                params.stopwatchId,
+                "pausedTimeCount",
+                newTime
+            );
+
+            store.setCell("stopwatches", params.stopwatchId, "paused", false);
+
+            store.setCell("stopwatches", params.stopwatchId, "lastPausedTime", -1);
+        });
+
+        // currentTime = Date.now();
+    }
+    function pause() {
+        const now = Date.now()
+        store.transaction(() => {
+            store.setCell('stopwatches', params.stopwatchId, 'lastPausedTime', now)
+            store.setCell('stopwatches', params.stopwatchId, 'paused', true)
+        })
+    }
+
+    const controlButtonCSS = "border-2 border-black dark:border-neutral-300 rounded-md text-white  min-w-24 px-1 py-1 pb-1.5"
+    return (
+        <div className="flex gap-3 items-center justify-evenly flex-wrap">
+            {paused ? (
+                <button className="control-button bg-lime-700" onClick={unpause}>
+                    Resume
+                </button>
+            ) : (
+                <button
+                    className="control-button bg-rose-700"
+                    onClick={pause}
+                >
+                    Pause
+                </button>
+            )}
+        </div>
+    );
+}
 
 function msToUnits(ms: number) {
     return {
@@ -80,14 +173,14 @@ function msToUnits(ms: number) {
         minutes: Math.floor((ms / (1000 * 60)) % 60),
         hours: Math.floor((ms / (1000 * 60 * 60)) % 24),
         days: Math.floor(ms / (1000 * 60 * 60 * 24)),
-    }
+    };
 }
 function unitsToDisplayStrings(elapsedTimeUnits: ReturnType<typeof msToUnits>) {
     return {
-        milliseconds: elapsedTimeUnits.milliseconds.toString().padStart(3, '0'),
-        seconds: elapsedTimeUnits.seconds.toString().padStart(2, '0'),
-        minutes: elapsedTimeUnits.minutes.toString().padStart(2, '0'),
-        hours: elapsedTimeUnits.hours.toString().padStart(2, '0'),
-        days: elapsedTimeUnits.days.toString().padStart(2, '0'),
-    }
+        milliseconds: elapsedTimeUnits.milliseconds.toString().padStart(3, "0"),
+        seconds: elapsedTimeUnits.seconds.toString().padStart(2, "0"),
+        minutes: elapsedTimeUnits.minutes.toString().padStart(2, "0"),
+        hours: elapsedTimeUnits.hours.toString().padStart(2, "0"),
+        days: elapsedTimeUnits.days.toString().padStart(2, "0"),
+    };
 }
--- stopwatch-tracker-react/src/main.tsx
+++ stopwatch-tracker-react/src/main.tsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './lib/App.tsx'
 
+import "@fontsource/noto-mono" 
+import "@fontsource/ibm-plex-mono" 
+import '@fontsource-variable/inter';
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
