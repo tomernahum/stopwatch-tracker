624bd5d58b5ac63a0f381913ff10eed9124ab6d7
tomer <101682824+tomernahum@users.noreply.github.com>
2024-12-07 13:19:16 -0500

    info section

--- stopwatch-tracker-react/src/main.tsx
+++ stopwatch-tracker-react/src/main.tsx
@@ -1,8 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+// import './index.css'
 import App from './lib/App.tsx'
 
+// TODO put in index.css so you don't get flash of unstyled fonts wating for js to run (i think that would wourk)
 import "@fontsource/noto-mono" 
 import '@fontsource-variable/inter';
 
